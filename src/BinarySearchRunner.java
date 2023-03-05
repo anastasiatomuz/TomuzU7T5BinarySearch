fa4b485116c0704d503eec0205cab0bfe2c08dc6
Anastasia <atomuz1957@bths.edu>
2023-03-04 22:55:46 -0500

    ec commit

--- src/BinarySearchRunner.java
+++ src/BinarySearchRunner.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class BinarySearchRunner
 {
     public static void main(String[] args)
@@ -46,7 +48,7 @@ public class BinarySearchRunner
         // When you are ready, uncomment each of these tests and run to see printed commentary
         // alongside every search; this prints out what is being checked and what is happening
 
-        int[] arr1 = {-10, -6, 0, 2, 12, 14, 17, 23, 35, 45, 49, 55, 81, 100, 108};
+        //int[] arr1 = {-10, -6, 0, 2, 12, 14, 17, 23, 35, 45, 49, 55, 81, 100, 108};
 /*
     // test when the target is in the middle (found on the first try!)
     int index = BinarySearch.binarySearchPrintedCommentary(arr1, 23);
@@ -95,5 +97,26 @@ public class BinarySearchRunner
     System.out.println(index);
     System.out.println("----------------------------------------------------");
 */
+
+//        int[] arr1 = {-47, -35, -27, -20, -14, -4, 6, 10, 14, 22};
+//        System.out.println(Arrays.binarySearch(arr1, -27));
+//        int[] arr2 = {-46, -45, -19, -9, 24, 26, 38, 38, 43};
+//        System.out.println(Arrays.binarySearch(arr2, -19));
+//        int[] arr3 = {-168, -120, -84, -79, -26, -6, -5, -1, 5, 22, 58, 62, 75, 94, 153, 186, 187};
+//        System.out.println(Arrays.binarySearch(arr3, -84));
+//        int[] arr4 = {-10, -6, 0, 2, 12, 14, 17, 23, 35, 45, 49, 55, 81, 100, 108};
+//        System.out.println(Arrays.binarySearch(arr4, 0));
+
+
+        //uses overloaded BinarySearch with Object parameters
+        String[] strArr1 = {"quote", "crossing", "forbid", "sticky", "barrel", "cheat", "basic", "belly"};
+        Arrays.sort(strArr1);
+        String[] strArr2 = {"digital", "philosophy", "crystal", "graduate", "precede", "retiree", "entitlement", "explode"};
+        Arrays.sort(strArr2);
+        System.out.println(Arrays.binarySearch(strArr1, "cheat"));
+        System.out.println(Arrays.binarySearch(strArr1, "quote"));
+        System.out.println(Arrays.binarySearch(strArr2, "philosophy"));
+        System.out.println(Arrays.binarySearch(strArr2, "happy"));
+
     }
 }
